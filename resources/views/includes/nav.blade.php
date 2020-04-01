a0724a7be8799955e16400a7986d25fee0079573
Hein Thanth <official.heinthanth@gmail.com>
2020-04-01 20:42:46 +0630

    ✨ Add Google Analytics and April Fool Message Box
    add google analytics for traffic and april fool message box for fun

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,6 +1,7 @@
 <li class="h3x-side-nav-item"><a href="{{ route('home') }}" data-render-me="true">HOME</a></li>
 <li class="h3x-side-nav-item"><a href="{{ route('about') }}" data-render-me="true">ABOUT</a></li>
 <li class="h3x-side-nav-item"><a href="{{ route('skills') }}" data-render-me="true">SKILLS</a></li>
+<li class="h3x-side-nav-item"><a href="{{ route('april-fool') }}" data-render-me="true">APRIL</a></li>
 {{-- <li class="h3x-side-nav-item"><a href="#" data-render-me="true">PORTFOLIO</a></li> --}}
 {{-- <li class="h3x-side-nav-item"><a href="#" data-render-me="true">BLOG</a></li> --}}
 {{-- <li class="h3x-side-nav-item"><a href="/contact" data-render-me="true">CONTACT</a></li> --}}
